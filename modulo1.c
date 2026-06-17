f1e076634bf15f89663942bc2acd2b7a6329912e
joaol2008g <joaol.2008@alunos.utfpr.edu.br>
2026-06-17 18:34:14 -0300

    Update modulo1.c

--- modulo1.c
+++ modulo1.c
@@ -5,6 +5,13 @@
 #include <math.h>
 #define MAX 20
 
+float somaVetor(float v[], int *n);
+float mediaVetor(float v[], int *n);
+float varianciaVetor(float v[], int *n);
+float desvioVetor(float v[], int *n);
+float medianaVetor(float v[], int *n);
+float produtoVetor(float v[], int *n);
+
 void lerVetor(float v[], int *n){
     
     int i;
@@ -21,27 +28,44 @@ void lerVetor(float v[], int *n){
         
 
 for(i = 0; i < *n; i++){
-    printf("Elemento [%d]: " , i);
+    printf("Elemento [%d]:  " , i);
     scanf("%f", &v[i]);
+      
     
 }
 
 }
 void exibirVetor(float v[], int *n){
+   
     
-
     int i = 0;
    printf("Vetor:");
     while(i  < *n){
  
-   printf(" %3.f", v[i]);
+   printf(" %.2f", v[i]);
     i++;
     
     }
     printf("\n");
+     float variancia =  varianciaVetor(v, n);
+    float desvio = desvioVetor(v, n);
+    float soma = somaVetor(v, n);
+    float media = mediaVetor(v, n);
+    float mediana = medianaVetor(v, n);
+    float produto = produtoVetor(v, n);
     
-   
-
+    printf("Soma: %.2f", soma);
+    printf("\n");
+    printf("Produto:  %.2f", produto);
+    printf("\n");
+    printf("Média:  %.2f", media);
+     printf("\n");
+    printf("Variância:  %.2f", variancia);
+    printf("\n");
+    printf("Desvio Padrão: %.2f ", desvio);
+    printf("\n");
+    printf("Mediana:  %.2f", mediana);
+printf("\n");
     
     
 }
@@ -51,52 +75,25 @@ float somaVetor(float v[], int *n){
     float soma;
     
     int i = 0;
-    soma = 0;
+    soma = 0.0;
     
      
     while(i  < *n){
     soma = soma + v[i];
     i++;
     }
-    printf("Soma: %2.f \n", soma);
-    
-    float produto;
-    
-     i = 0;
-    produto = 1;
-    
-     
-    while(i  < *n){
-    produto = produto * v[i];
-    i++;
-   
-
-    }
-    printf("\n");
-    printf("Produto: %2f \n", produto);
     
     return soma;
 
     
 }
 float mediaVetor(float v[], int *n){
+
+    float soma = somaVetor(v, n);
     
-    int i;
-    float soma;
-     soma = 0;
     
+    float media = soma / *n;
      
-    while(i  < *n){
-    soma = soma + v[i];
-    i++;
-    }
-        
-    float media;
-    
-    media = soma / *n;
-    
-    printf("Media: %f \n" , media);
-    
     
     
     return media;
@@ -104,25 +101,13 @@ float mediaVetor(float v[], int *n){
     
 }
 float varianciaVetor(float v[], int *n){
-      float variancia;
+    
+   
+    float variancia;
     float variancia1 = 0;
-    float variancia2;
-    
-   int i;
-    float soma;
-     soma = 0;
-    
-     
-    while(i  < *n){
-    soma = soma + v[i];
-    i++;
-    }
-        
-    float media;
-    
-    media = soma / *n;
-    
-    
+    float variancia2 = 0;
+    float media = mediaVetor(v, n);
+
     for(int i = 0; i < *n; i++){
         
         variancia2 = (v[i] - media) * (v[i] - media);
@@ -131,93 +116,74 @@ float varianciaVetor(float v[], int *n){
     }
     
     variancia = variancia1 / (*n - 1);
+
+    return variancia;
     
-    printf("Variância: %f \n", variancia);
     
-    
-}
+} 
 float desvioVetor(float v[], int *n){
-        float variancia;
-    float variancia1 = 0;
-    float variancia2;
+
+    float variancia = varianciaVetor(v, n);
     
-   int i;
-    float soma;
-     soma = 0;
-    
-     
-    while(i  < *n){
-    soma = soma + v[i];
-    i++;
-    }
-        
-    float media;
-    
-    media = soma / *n;
-    
-    
-    for(int i = 0; i < *n; i++){
-        
-        variancia2 = (v[i] - media) * (v[i] - media);
-        variancia1 = variancia2 + variancia1;
-        
-    }
-    
-    variancia = variancia1 / (*n - 1);
-    
-        
-    variancia  = varianciaVetor( v,  n);
-        
     float desviopadrao;
     
     desviopadrao = sqrt(variancia);
-    
-    printf("Desvio padrão: %f \n", desviopadrao);
-    
-}
+
+    return desviopadrao;
+    }
+
 float medianaVetor(float v[], int *n){
-     float mediana;
-    float temp;
-    int j;
-    for (j = 0; j < *n - 1; j++) {
-        
-        if (v[j] > v[j + 1]) {
-            temp = v[j];
-            v[j] = v[j + 1];
-            v[j + 1] = temp;
+    
+    float mediana;
+    float k;
+  
+    int i, j;
+    
+  
+    
+    for (i = 0; i < *n - 1; i++) {
+        for (j = 0; j < *n - i - 1; j++) {
+            if ( v[i] >  v[j + 1]) {
+                k =  v[i];
+                v[i] =  v[j + 1];
+                v[j + 1] = k;
+            }
         }
     }
-    if(*n%2 == 0){
-        mediana = (v[((int)*n/2)]  + v[((int)(*n/2 - 1))])/2;
-        printf("Mediana: %f", mediana);
+    
+   
+    if(*n % 2 == 0){
+        mediana = (v[*n / 2] + v[(*n / 2) - 1]) / 2;
     }
     else{
-       mediana = v[((int)*n/2)];
-       printf("Mediana: %f", mediana);
+       mediana = v[*n / 2];
     }
-    
     return mediana;
     
 }
-void minMaxVetor(float v[], int *n, float *min, float *max){
+
+
+
+
+void minMaxVetor(float v[], int *n, float min, float max){
     
-        int i; 
+    int i; 
         
-        int posicao;
+    int posicao;
     float confirmacao;
-    *max = v[0];
+    max = v[0];
 
     for(i = 1; i < *n; i++){
-        if(*max < v[i]){
-            *max = v[i];
+        if(max < v[i]){
+            max = v[i];
         }
     }
     
-    *min = v[0];
+    min = v[0];
 
     for(i = 1; i < *n; i++){
-        if(*min > v[i]){
-            *min = v[i];
+        if(min > v[i]){
+            min = v[i];
         }
     }
            
@@ -225,26 +191,33 @@ void minMaxVetor(float v[], int *n, float *min, float *max){
             
             
             
- printf("O menor valor é: %f e o Maior valor é: %f ", min, max);
+ printf("Maior valor: %f", max);
+ printf("\n");
+ printf("Menor valor: %f", min);
     
+}
+
+float produtoVetor(float v[], int *n){
+    
+    float produto = 1;
+    
+    for(int i = 0; i < *n; i++){
+        produto = v[i] * produto;
+    }
+    
+    return produto;
 }
 
 
 
-int main()
-{
+int main(){
     float v[MAX];
     int n;
     float min, max;
     
  lerVetor(v,  &n);
  exibirVetor( v, &n);
- somaVetor( v,  &n);
- mediaVetor( v, &n);
- varianciaVetor( v,  &n);
- desvioVetor( v,  &n);
- medianaVetor( v,  &n);
- minMaxVetor( v,  &n,  &min,  &max);
+ minMaxVetor( v,  &n,  min,  max);
 
 }
 
